0f8625b43a21758bd3a1db32abf074bf056d30ae
NIhal Patel <47061480+Nihal192@users.noreply.github.com>
2023-08-17 14:49:27 -0400

    Rotate_string.py
    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
    
    A shift on s consists of moving the leftmost character of s to the rightmost position.
    
    For example, if s = "abcde", then it will be "bcdea" after one shift.

---
+++ rotate_string.py
@@ -0,0 +1,7 @@
+# will be given goal and string so apde compare karisu  banev ne and then same na hoi to string na [1:] means ke 1th index par he char che ena thi start akrisu and s[0] index par je che ene append/concate karu daisu
+
+for i in range(len(s):
+  if (s!=goal):
+      s[1:]+s[0]
+      return(true)
+  return(true)
