44218830a6b89765e327f6c895f3eef919270723
NIhal Patel <47061480+Nihal192@users.noreply.github.com>
2023-08-29 20:52:28 -0400

    encode_decode.py

---
+++ encode_decode.py
@@ -0,0 +1,19 @@
+#encode function
+def encode(self,strs):
+  res=""  #variable to store result
+  for s in strs:
+    res+= str(len(strs))+"#"+s      #logic of preaprring string i.e  [2#hi]
+  return res
+
+#decode function
+def decode(self,str)
+res,i=[],0       #var res for result and i is for moving along each char in string .
+while i<len(n):    #check if you are out of bounds or not
+  j=i   #assign each value each other.
+  while str[j]!="#":   #sting ma check karso # have etle you know after this your string starts
+    length=int(str([i:j])    #ahiya you sayin afer # you go from string's 1st to last word .
+     res.append(str[j+1:j+1+length(j)])  #you are storing the result which is next letter of j nd len
+     i=j+1   #updating i
+  return res #return result
+    
+    
