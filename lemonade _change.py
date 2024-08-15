fd5bc21a4b5de2353c5dd2626b5fb3ad47a15963
Nihal192 <patelnihal56@gmail.com>
2024-08-14 22:37:40 -0400

    updated lemonade change problem code

--- lemonade _change.py
+++ lemonade _change.py
@@ -1,4 +1,13 @@
 def lemonade(bills):
     five=0
     ten=0
-    
+
+    for i in range(len(bills)):
+        if bills[i]==5:
+            five+=1
+        elif bills[i]==10:
+            if five>=1:
+                five-=1
+                ten+=1
+            else:
+                return False
