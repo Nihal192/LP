99ef75e85ae0ade4b0069218f0b4c68173734057
Nihal192 <patelnihal56@gmail.com>
2024-08-15 20:06:38 -0400

    updated senior citizen problem

--- senior_citize.py
+++ senior_citize.py
@@ -1,9 +1,9 @@
 data = ["7868190130M7522", "983498123F4522"]
 c=0
-for i in data:
-    age_Str=int(i[-4:-2])
+for i in data:   #this will run through the data list
+    age_Str=int(i[-4:-2])   #we are converting the list into string
 
     if age_Str > 60:
         c+=1
 
-print(c)
+print(c)    #return counter
