2e272c969a993461ef23b073a5c4fda231fc041a
NIhal Patel <47061480+Nihal192@users.noreply.github.com>
2025-02-06 16:19:56 -0500

    Create buy_sell_1.py
    another method

---
+++ buy_sell_1.py
@@ -0,0 +1,9 @@
+mp = float(inf)
+maxprofit = 0
+for i in range(len(nums):
+  if price[i] < mp:
+          mp = price[i]
+  elif:
+        price[i] - mp > maxprofit:
+        maxprofit = price[i] - mp
+return mp
